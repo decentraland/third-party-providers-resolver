89951b4b9f6c483d8605b552e6a917c0b0626274
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2023-06-06 19:21:04 -0300

    chore: typed handler response

--- src/controllers/handlers/get-providers-handler.ts
+++ src/controllers/handlers/get-providers-handler.ts
@@ -1,8 +1,14 @@
-import { HandlerContextWithPath } from '../../types'
+import { HandlerContextWithPath, ThirdPartyProvider } from '../../types'
+
+type GetProvidersResponse = {
+  body: {
+    thirdPartyProviders: ThirdPartyProvider[]
+  }
+}
 
 export async function getProvidersHandler(
   context: Pick<HandlerContextWithPath<'thirdPartyProvidersMemoryStorage', '/providers'>, 'url' | 'components'>
-) {
+): Promise<GetProvidersResponse> {
   const { thirdPartyProvidersMemoryStorage } = context.components
 
   const thirdPartyProviders = await thirdPartyProvidersMemoryStorage.get()
